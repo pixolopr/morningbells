72d6a47f3631b737365dcbe97813119e9f955560
Abhay Amin <pixoloproductions@gmail.com>
2015-11-17 19:33:38 +0530

    files

---
+++ belezza/js/main.js
@@ -0,0 +1,17 @@
+/*
+
+Pixolo Productions
+Bellezza Template
+For any business and offices
+Main.js
+
+*/
+
+/* FUNCIONS TO LOAD WHEN THE WEB PAGE IS READY */
+$(document).ready(function () {
+
+    /* PARALLAX */
+    /* This is done to load tha parallax effect, the parallax function is in the materialize.js*/
+    $('.homeimage').parallax();
+    
+});
